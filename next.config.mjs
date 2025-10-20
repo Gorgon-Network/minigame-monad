d3e47deb6fe47cc00b2b3605aed89d703ca43904
Hien <hackernovirus@gmail.com>
2025-10-20 16:05:29 +0700

    fix build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.resolve.fallback = {
+                ...config.resolve.fallback,
+                "@react-native-async-storage/async-storage": false,
+            };
+        }
+        return config;
+    },
+};
 
 export default nextConfig;
